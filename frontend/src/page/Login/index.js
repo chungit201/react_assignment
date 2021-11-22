2bed0da8d9a2fc3d7293f764f39a1ff6505699df
chungit201 <vuongvanchung30092001@gmail.com>
2021-11-22 14:53:57 +0700

    Test google auth

--- frontend/src/page/Login/index.js
+++ frontend/src/page/Login/index.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react'
-import {useForm} from 'react-hook-form';
 import {signin} from '../../api/authApi';
 import BtnLogin from '../../component/BtnLogin';
 import {Input} from "antd";
