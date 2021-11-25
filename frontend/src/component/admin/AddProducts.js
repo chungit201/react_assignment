115e9787650c3bed3b0b26c77a7aabf04938e5d5
chungit201 <vuongvanchung30092001@gmail.com>
2021-11-25 07:40:30 +0700

    edit core

--- frontend/src/component/admin/AddProducts.js
+++ frontend/src/component/admin/AddProducts.js
@@ -1,80 +1,93 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { addProduct } from '../../api/productApi';
 import { ToastContainer, toast } from "react-toastify";
-import {getAll} from "../../api/categoriesApi";
+import { getAll } from "../../api/categoriesApi";
+import { Form } from 'react-bootstrap';
 const AddProduct = () => {
-    const [products, setProducts] = useState([]);
-    const {
-        register,
-        handleSubmit,
-    } = useForm();
+  const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState();
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const { data } = await getAll();
+        setCategory(data);
 
-    const onSubmit = async (product) => {
-        console.log(product);
-        const { data } = await addProduct(product);
-        setProducts([...products, data]);
-        toast.success("Thêm sản phẩm thành công");
-        window.location.href='/admin/list-products';
-    }
-    useEffect(()=>{
-        const getCategories =  async () =>{
-            const {data} = await getAll()
-        }
-    },[])
-    return (
-        <div className="row container">
-            <div className="col-12">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    {/* 2 column grid layout with text inputs for the first and last names */}
-                    <div className="row mb-4">
-                        <div className="col">
-                            <div className="form-outline">
-                                <label className="form-label" htmlFor="form6Example1">Categories</label>
-                                <input type="text" id="form6Example1" className="form-control" {...register('categories')} />
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getCategories();
+  }, []);
+  const {
+    register,
+    handleSubmit,
+  } = useForm();
 
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="form-outline">
-                                <label className="form-label" htmlFor="form6Example2">Product name</label>
-                                <input type="text" id="form6Example2" className="form-control"  {...register('name')} />
-
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="row mb-4">
-                        <div className="col">
-                            <div className="form-outline">
-                                <label className="form-label" htmlFor="form6Example1">Price</label>
-                                <input type="number" id="form6Example1" className="form-control"  {...register('price')} />
-
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="form-outline">
-                                <label className="form-label" htmlFor="form6Example2">Image</label>
-                                <input type="text" id="form6Example2" className="form-control" {...register('img')} />
-
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="form-outline mb-4">
-                        <label className="form-label" htmlFor="form6Example5">Introduce</label>
-                        <input type="text" id="form6Example5" className="form-control" {...register('introduce')} />
-
-                    </div>
-                    <div className="form-outline mb-4">
-                        <label className="form-label" htmlFor="form6Example7">Description</label>
-                        <textarea className="form-control" id="form6Example7" rows={4} defaultValue={""} {...register('description')} />
-                    </div>
-                    <button type="submit" className="btn btn-primary btn-block mb-4">Save</button>
-                </form>
+  const onSubmit = async (product) => {
+    console.log(product);
+    const { data } = await addProduct(product);
+    setProducts([...products, data]);
+    toast.success("Thêm sản phẩm thành công");
+    window.location.href = '/admin/list-products';
+  }
+  return (
+    <div className="row container">
+      <div className="col-12">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* 2 column grid layout with text inputs for the first and last names */}
+          <div className="row mb-4">
+            <div className="col">
+              <div className="form-outline">
+                <label className="form-label" htmlFor="form6Example1">Categories</label>
+                {/* <select type="text" id="form6Example1" className="form-control" {...register('categories')} /> */}
+                <Form.Select aria-label="Default select example" size="lg">
+                  {category && (category.results.map(item => {
+                    return <option value={item.id}>{item.name}</option>
+                  }))}
+                </Form.Select>
+              </div>
             </div>
-        </div>
-    )
+            <div className="col">
+              <div className="form-outline">
+                <label className="form-label" htmlFor="form6Example2">Product name</label>
+                <input type="text" id="form6Example2" className="form-control"  {...register('name')} />
+
+              </div>
+            </div>
+          </div>
+
+          <div className="row mb-4">
+            <div className="col">
+              <div className="form-outline">
+                <label className="form-label" htmlFor="form6Example1">Price</label>
+                <input type="number" id="form6Example1" className="form-control"  {...register('price')} />
+
+              </div>
+            </div>
+            <div className="col">
+              <div className="form-outline">
+                <label className="form-label" htmlFor="form6Example2">Image</label>
+                <input type="text" id="form6Example2" className="form-control" {...register('img')} />
+
+              </div>
+            </div>
+          </div>
+
+          <div className="form-outline mb-4">
+            <label className="form-label" htmlFor="form6Example5">Introduce</label>
+            <input type="text" id="form6Example5" className="form-control" {...register('introduce')} />
+
+          </div>
+          <div className="form-outline mb-4">
+            <label className="form-label" htmlFor="form6Example7">Description</label>
+            <textarea className="form-control" id="form6Example7" rows={4} defaultValue={""} {...register('description')} />
+          </div>
+          <button type="submit" className="btn btn-primary btn-block mb-4">Save</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default AddProduct;
--- frontend/src/page/ProductDetail/ProductDetail.js
+++ frontend/src/page/ProductDetail/ProductDetail.js
@@ -28,28 +28,28 @@ const ProductDetail = () => {
           <img className="img_detail" src={product && product.img} />
         </div>
         <div className="col-6">
-            <div className="introduct">
+          <div className="introduct">
             <h2>{product && product.name}</h2>
-          <h3 className="price"><NumberFormat thousandSeparator={true} thousandsGroupStyle="vnd"
-            value={product && product.price} suffix={' VND'} /></h3>
-          <span>Hướng dẫn chọn size:</span>
-          <div className="list-size">
-            <a href="" className="nofollow">M</a>
-            <a href="" className="nofollow">L</a>
-            <a href="" className="nofollow">XL</a>
-            <div className="quantity">
-              <p>Số lượng :</p>
-              <input className="ip_quantity" type="number" />
+            <h3 className="price"><NumberFormat thousandSeparator={true} thousandsGroupStyle="vnd"
+              value={product && product.price} suffix={' VND'} /></h3>
+            <span>Hướng dẫn chọn size:</span>
+            <div className="list-size">
+              <a href="" className="nofollow">M</a>
+              <a href="" className="nofollow">L</a>
+              <a href="" className="nofollow">XL</a>
+              <div className="quantity">
+                <p>Số lượng :</p>
+                <input className="ip_quantity" type="number" />
+              </div>
+            </div>
+
+            <p className="description">{product && product.description}</p>
+
+            <div className="group_btn">
+              <Button variant="outline-danger">Thêm vào giỏ hàng</Button>{' '}
+              <Button variant="outline-dark" >Mua ngay</Button>{' '}
             </div>
           </div>
-
-          <p className="description">{product && product.description}</p>
-
-          <div className="group_btn">
-            <Button variant="outline-danger">Thêm vào giỏ hàng</Button>{' '}
-            <Button variant="outline-dark" >Mua ngay</Button>{' '}
-          </div>
-            </div>
         </div>
       </div>
     </div>
